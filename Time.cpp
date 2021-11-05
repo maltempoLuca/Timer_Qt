10af312a1d7c49bd55911cb3c1f8b133027e80ec
maltempoLuca <maltempoluca98@gmail.com>
2021-11-05 10:52:14 +0100

    Aggiunta la classe Time.cpp/.h per rappresentare la data.

---
+++ Time.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Luca Maltempo on 05/11/21.
+//
+
+#include "Time.h"
+
+Time::Time(int milliSeconds, int seconds, int minutes, int hours, TimeFormat timeFormat) : milliSeconds(milliSeconds),
+                                                                                           seconds(seconds),
+                                                                                           minutes(minutes),
+                                                                                           hours(hours),
+                                                                                           timeFormat(timeFormat) {}
+
+int Time::getMilliSeconds() const {
+    return milliSeconds;
+}
+
+void Time::setMilliSeconds(int milliSeconds) {
+    Time::milliSeconds = milliSeconds;
+}
+
+int Time::getSeconds() const {
+    return seconds;
+}
+
+void Time::setSeconds(int seconds) {
+    Time::seconds = seconds;
+}
+
+int Time::getMinutes() const {
+    return minutes;
+}
+
+void Time::setMinutes(int minutes) {
+    Time::minutes = minutes;
+}
+
+int Time::getHours() const {
+    return hours;
+}
+
+void Time::setHours(int hours) {
+    Time::hours = hours;
+}
+
+
+
---
+++ Time.h
@@ -0,0 +1,40 @@
+//
+// Created by Luca Maltempo on 05/11/21.
+//
+
+#ifndef TIMER_QT_TIME_H
+#define TIMER_QT_TIME_H
+
+enum TimeFormat {
+    Format12h, Format24h
+};
+
+class Time {
+public:
+    Time(int milliSeconds, int seconds, int minutes, int hours, TimeFormat = Format24h);
+
+public:
+    int getMilliSeconds() const;
+
+    void setMilliSeconds(int milliSeconds);
+
+    int getSeconds() const;
+
+    void setSeconds(int seconds);
+
+    int getMinutes() const;
+
+    void setMinutes(int minutes);
+
+    int getHours() const;
+
+    void setHours(int hours);
+
+private:
+    int milliSeconds, seconds, minutes, hours;
+    TimeFormat timeFormat;
+
+};
+
+
+#endif //TIMER_QT_TIME_H
