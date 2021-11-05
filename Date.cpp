afed82d6225115337bb4b260d679ba864b415db8
maltempoLuca <maltempoluca98@gmail.com>
2021-11-05 10:32:18 +0100

    Aggiunta la classe Date.cpp/.h per rappresentare la data.

---
+++ Date.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Luca Maltempo on 05/11/21.
+//
+
+#include "Date.h"
+
+Date::Date(int day, int dayOfWeek, int month, int year, bool isLeapYear) : day(day), dayOfWeek(dayOfWeek), month(month),
+                                                                           year(year), isLeapYear(isLeapYear) {}
+
+int Date::getDay() const {
+    return day;
+}
+
+void Date::setDay(int day) {
+    Date::day = day;
+}
+
+int Date::getDayOfWeek() const {
+    return dayOfWeek;
+}
+
+void Date::setDayOfWeek(int dayOfWeek) {
+    Date::dayOfWeek = dayOfWeek;
+}
+
+int Date::getMonth() const {
+    return month;
+}
+
+void Date::setMonth(int month) {
+    Date::month = month;
+}
+
+int Date::getYear() const {
+    return year;
+}
+
+void Date::setYear(int year) {
+    Date::year = year;
+}
+
+bool Date::isLeapYear1() const {
+    return isLeapYear;
+}
+
+void Date::setIsLeapYear(bool isLeapYear) {
+    Date::isLeapYear = isLeapYear;
+}
+
+
+Date::~Date() = default;
---
+++ Date.h
@@ -0,0 +1,46 @@
+//
+// Created by Luca Maltempo on 05/11/21.
+//
+
+#ifndef TIMER_QT_DATE_H
+#define TIMER_QT_DATE_H
+
+#include <string>
+
+class Date {
+public:
+    Date(int day, int dayOfWeek, int month, int year, bool isLeapYear);
+
+    ~Date();
+
+    int getDay() const;
+
+    void setDay(int day);
+
+    int getDayOfWeek() const;
+
+    void setDayOfWeek(int dayOfWeek);
+
+    int getMonth() const;
+
+    void setMonth(int month);
+
+    int getYear() const;
+
+    void setYear(int year);
+
+    bool isLeapYear1() const;
+
+    void setIsLeapYear(bool isLeapYear);
+
+
+private:
+    int day, dayOfWeek, month, year;
+    bool isLeapYear;
+    std::string daysOfWeek[7] = {"Monday", "Tuesday", "Wednesday",
+                                 "Thursday", "Friday", "Saturday",
+                                 "Sunday"};
+};
+
+
+#endif //TIMER_QT_DATE_H
