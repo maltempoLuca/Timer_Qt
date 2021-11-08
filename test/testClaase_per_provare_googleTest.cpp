bded70a4e91fb4d87328e64d5b0fab99f534c7c1
maltempoLuca <maltempoluca98@gmail.com>
2021-11-08 18:28:23 +0100

    Rimosse i file .cpp e .h della classe usata per testare il funzionamento di google test.

--- test/testClaase_per_provare_googleTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-#include "../Headers/claase_per_provare_googleTest.h"
-
-TEST(claase_per_provare_googleTest, costruttore) {
-    claase_per_provare_googleTest cappero(5);
-    ASSERT_EQ(5, cappero.getNumero());
-    ASSERT_NE('PIPPO', cappero.getNumero());
-}
