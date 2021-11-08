07b653966f9e1b31b1f22f051b2aa6219d1547d9
maltempoLuca <maltempoluca98@gmail.com>
2021-11-08 19:05:39 +0100

    Aggiunta la macro: ASSERT_EXCEPTION per poter effettuare i test di inserimento dei valori in Date.cpp

--- test/testDate.cpp
+++ test/testDate.cpp
@@ -1,9 +1,29 @@
 #include <QDate>
 #include "gtest/gtest.h"
-
 #include "../Headers/Date.h"
 
-TEST(Date, DefaultConstructor) {
+#define ASSERT_EXCEPTION(TRY_BLOCK, EXCEPTION_TYPE, MESSAGE){             \
+    try                                                                   \
+    {                                                                     \
+        TRY_BLOCK                                                         \
+        FAIL() << "exception '" << MESSAGE << "' not thrown at all!";     \
+    }                                                                     \
+    catch( const EXCEPTION_TYPE& e )                                      \
+    {                                                                     \
+        ASSERT_STREQ( MESSAGE, e.what() )                                 \
+            << " exception message is incorrect. Expected the following " \
+               "message:\n\n"                                             \
+            << MESSAGE << "\n";                                           \
+    }                                                                     \
+    catch( ... )                                                          \
+    {                                                                     \
+        FAIL() << "exception '" << MESSAGE                                \
+               << "' not thrown with expected type '" << #EXCEPTION_TYPE  \
+               << "'!";                                                   \
+    }                                                                     \
+}
+
+TEST(DateTest, DefaultConstructor) {
     Date date;
     QDate qDate = QDate::currentDate();;
     ASSERT_EQ(qDate.day(), date.getDay());
@@ -12,13 +32,9 @@ TEST(Date, DefaultConstructor) {
     ASSERT_EQ(qDate.year(), date.getYear());
 }
 
-TEST(Date, InsertWringValue) {
+TEST(DateTest, InsertingWrongDay) {
     Date date;
-    QDate qDate = QDate::currentDate();;
-    ASSERT_EQ(qDate.day(), date.getDay());
-    ASSERT_EQ(qDate.dayOfWeek() - 1, date.getDayOfWeek());
-    ASSERT_EQ(qDate.month(), date.getMonth());
-    ASSERT_EQ(qDate.year(), date.getYear());
+    ASSERT_EXCEPTION(date.setDay(0);, std::invalid_argument, "Invalid day");
 }
 
 
